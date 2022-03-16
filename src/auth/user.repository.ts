b7e63d521fc5d2cec3e044ba03e5ee679f06765d
ctlam <lamisreal>
2022-03-16 10:12:35 +0700

    fetch: check dulicate user name and hash password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,39 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRespository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    console.log(salt);
+
     const user = new User();
     user.username = username;
-    user.password = password;
-    await user.save();
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    console.log(user.password);
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        //dulicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
